47fc8db7aa09da0fec7bd41c5b78891711b8e84b
Pedro Arthur Fernandes de Vasconcelos <pfernandesvasconcelos@gmail.com>
2016-12-22 02:40:33 -0300

    added scrolspy and a carousel at the intro section

--- mostrapei.js
+++ mostrapei.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
 
-	 $(".button-collapse").sideNav({
+	$(".button-collapse").sideNav({
 
-	 	menuWidth: 300, // Default is 240
+	 	menuWidth: 240, // Default is 240
 	    edge: 'right', // Choose the horizontal origin
 	    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
 	    draggable: true // Choose whether you can drag to open on touch screens
 
-	 });
+	});
+
+	$('.scrollspy').scrollSpy({
+	 	scrollOffset: 0
+	});
+
+    $('.carousel.carousel-slider').carousel({full_width: true});
+
+
+    $('.carousel').carousel({dist:0});
+
+    window.setInterval(function() {
+    	$('.carousel').carousel('next')
+    }, 4000);
 
 });
